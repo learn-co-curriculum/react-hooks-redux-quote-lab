400fab5e8705f3422130974b3624f12ee1cbf9b1
Lukeghenco <lukeghenco@gmail.com>
2017-08-18 09:07:58 -0400

    Update Store.js

--- src/store.js
+++ src/store.js
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 import rootReducer from './reducers/index'
 
 export function configureStore(){
-  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(
+    rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
 
 export const store = configureStore()
