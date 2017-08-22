e7f2af40c4e4f545c3c1ad0a8210c510c450589c
Lukeghenco <lukeghenco@gmail.com>
2017-08-22 17:53:30 -0400

    Remove unused modules from Store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,5 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './reducers/index'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk'
 
 export function configureStore(){
   return createStore(
